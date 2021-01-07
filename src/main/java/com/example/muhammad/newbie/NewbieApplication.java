2957f64a95c0ccdbbda546f176c6da10fa149f55
Kajol232 <ridamot123@yahoo.com>
2021-01-07 17:47:35 +0100

    newbie v1

---
+++ src/main/java/com/example/muhammad/newbie/NewbieApplication.java
@@ -0,0 +1,13 @@
+package com.example.muhammad.newbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbieApplication.class, args);
+    }
+
+}
